c68d61d4b616cad85495b7599bd994b1912b01bf
Melissa Mendez <mmmeldz@gmail.com>
2018-05-14 21:14:32 -0400

    Events

--- src/App.js
+++ src/App.js
@@ -10,20 +10,53 @@ import React, { Component } from 'react';
         { description: 'Feed the kittens', isCompleted: true },
         { description: 'Take Kittens for a walk', isCompleted: false },
         { description: 'Give kittens a bubble bath', isCompleted: false }
-      ]
+      ],
+       newTodoDescription: ''
     };
-   }
+       this.deleteTodo = this.deleteTodo.bind(this);
+}
+
+deleteTodo(description) {
+          const filteredTodos = this.state.todos.filter((todo, index) =>  todo.description !== description);
+            this.setState({
+              todos: filteredTodos
+            });
+          }
+
+   handleChange(e) {
+       this.setState({ newTodoDescription: e.target.value })
+     }
+
+   handleSubmit(e) {
+        e.preventDefault();
+         if (!this.state.newTodoDescription) { return }
+        const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+        this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+      }
+
+   toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
+  }
+
    render() {
      return (
        <div className="App">
         <ul>
         { this.state.todos.map( (todo, index) =>
-        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
-          )}
-        </ul>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteToDo={this.deleteTodo} />
+           )}
+         </ul>
+          <form onSubmit={ (e) => this.handleSubmit(e) }>
+           <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
        </div>
      );
    }
  }
+
 
  export default App;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class ToDo extends Component {
+  deleteToDo(description) {
+  this.props.deleteToDo(description);
+}
   render() {
     return (
-    <li>
-      <input type="checkbox" checked={ this.props.isCompleted } />
-      <span>{ this.props.description }</span>
-       </li>
-    );
-  }
-}
+      <div className="wrapper">
+          <button className="deleteToDo" onClick = {(e) =>
+            this.deleteToDo(this.props.description)}>Delete</button>
+            {() => this.props.deleteToDo(this.props.description)}
+
+          <li>
+            <input type="checkbox" checked={ this.props.isCompleted }
+            onChange={ this.props.toggleComplete } />
+            <span>{ this.props.description }</span>
+          </li>
+          </div>
+        );
+      }
+    }
 
 export default ToDo;
