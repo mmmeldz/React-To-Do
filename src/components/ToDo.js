06210e9dc80b5df08ac25ca2aa7a6c456954b9f6
Melissa Mendez <mmmeldz@gmail.com>
2018-05-20 14:29:19 -0400

    corrected ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 
 class ToDo extends Component {
-  deleteToDo(description) {
-  this.props.deleteToDo(description);
-}
   render() {
     return (
-      <div className="wrapper">
-          <button className="deleteToDo" onClick = {(e) =>
-            this.deleteToDo(this.props.description)}>Delete</button>
-            {() => this.props.deleteToDo(this.props.description)}
-
-          <li>
-            <input type="checkbox" checked={ this.props.isCompleted }
-            onChange={ this.props.toggleComplete } />
-            <span>{ this.props.description }</span>
-          </li>
-          </div>
-        );
-      }
-    }
+    <li>
+    <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+      <span>{ this.props.description }</span>
+       </li>
+    );
+  }
+}
 
 export default ToDo;
